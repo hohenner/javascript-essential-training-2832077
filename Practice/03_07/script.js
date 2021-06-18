2b85fc4ae8a41d8651c4347cea655bf10be60793
Andy Hohenner <ahohenner@mdsol.com>
2021-06-18 11:53:30 -0700

    first practice, creating objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,31 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const drink = {
+    type: "tea",
+    temperature: "hot",
+    changeTemp: function (temp) {
+        this.temperature = temp
+    },
+}
+const cup = {
+    color: "blue",
+    handle: true,
+    volume: 10,
+    writing: false,
+    picture: false,
+    full: true,
+    contents: drink,
+    emptyCup: function () {
+        this.full = false;
+        this.contents = null;
+      },
+    fillCup: function () {
+        this.full = true;
+        this.contents = drink;
+    }
+}
+
+console.log("The cup object:", cup);
+console.log("drink is ",drink)
