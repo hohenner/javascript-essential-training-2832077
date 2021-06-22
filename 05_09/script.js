0761fcba61def3765d6156b43fc700fc6724d3c2
Andy Hohenner <ahohenner@mdsol.com>
2021-06-22 14:10:23 -0700

    05-09: adding doc link

--- 05_09/script.js
+++ 05_09/script.js
@@ -2,6 +2,7 @@
  * Traverse the DOM tree using querySelector() and querySelectorAll()
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelectorAll
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
  */
 
 import Backpack from "./Backpack.js";
