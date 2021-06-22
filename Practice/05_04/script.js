7d1ff20a2147845adb6af0fae18ae0b28c074f47
Andy Hohenner <ahohenner@mdsol.com>
2021-06-22 11:13:59 -0700

    Practice 05-04: searching for DOM elements

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,22 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+console.log("siteheader class:", document.querySelector(".siteheader"));
+
+console.log(
+  "siteheader by getElementsByClass:",
+  document.getElementsByClassName("siteheader")
+);
+
+var features = document.querySelectorAll(".feature");
+
+features.forEach((item) => (item.style.backgroundColor = "lightblue"));
+
+var backpack_features = document.querySelectorAll(".backpack__features");
+
+backpack_features.forEach(
+  (item) =>
+    (item.querySelector(".backpack__volume").style.backgroundColor =
+      "lightsteelblue")
+);
