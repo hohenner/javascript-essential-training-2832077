e885f8126ae1751a87f93b2226d5fe7de67eea0f
Andy Hohenner <ahohenner@mdsol.com>
2021-09-07 10:59:29 -0700

    exercise 06_02 creating variables

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,14 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+
+var container = 5;
+container = "red";
+
+something = 7; /* will throw errors in IDE but allowed */
+
+var x = 4,
+  y = 5,
+  z = "blue";
+
+var empty;
