59ce66e57ce535a0878b5739e8e3892ec7244698
Andy Hohenner <ahohenner@mdsol.com>
2021-09-08 11:30:12 -0700

    array methods

--- 07_03/script.js
+++ 07_03/script.js
@@ -6,15 +6,26 @@
 let backpackContents = ["piggy", "headlamp", "pen"];
 
 console.log(backpackContents);
+console.log(backpackContents.join(", "));
 
-// backpackContents.forEach(function (item) {
-//   item = `<li>${item}</li>`;
-//   console.log(item);
-// });
+backpackContents.push("pencil", 5);
+console.log(backpackContents.join(", "));
+backpackContents.unshift("pencil", 5);
+console.log(backpackContents.join(", "));
+console.log(backpackContents.shift());
+console.log(backpackContents.join(", "));
+console.log(backpackContents.pop());
+console.log(backpackContents.join(", "));
 
-// let longItems = backpackContents.find(function (item) {
-//   if (item.length >= 5) {
-//     return item;
-//   }
-// });
-// console.log("longItems:", longItems);
+backpackContents.forEach(function (item) {
+  item = `<li>${item}</li>`;
+  console.log(item);
+});
+console.log(backpackContents.join(", "));
+let longItems = backpackContents.find(function (item) {
+  if (item.length >= 5) {
+    return item;
+  }
+});
+console.log("longItems:", longItems);
+console.log(backpackContents.join(", "));
